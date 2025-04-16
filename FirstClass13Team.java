4fd9ee75c9fb92fb192e9b320b9f9c6a17582ff4
-------- <imagine.zero.2020@gmail.com>
2025-04-16 20:31:43 +0800

    完成main程式

---
+++ FirstClass13Team.java
@@ -0,0 +1,6 @@
+public class FirstClass13Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257034\"\n姓名：\"黃彥翔\"");
+    }
+}
