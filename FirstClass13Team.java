02d015a86780e2a1dbd9de8c79bd5e470626fada
jua-da <abarrywang1023@gmail.com>
2025-04-16 20:48:13 +0800

    myname

--- FirstClass13Team.java
+++ FirstClass13Team.java
@@ -2,5 +2,6 @@ public class FirstClass13Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257034\"\n姓名：\"黃彥翔\"");
+        System.out.println("\"學號：\"01257065\"\n姓名：\"王大倬\"");
     }
 }
